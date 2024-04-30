fcc53739005ba1f52a429a71b60d21fc6b591e40
User <User@LAPTOP-O7CLEELI>
2024-04-30 05:48:13 +0400

    Add project files.

---
+++ LabTask28/Program.cs
@@ -0,0 +1,17 @@
+﻿using LabTask28;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        List<Exam> exams = new List<Exam>();
+
+        Exam exam = new Exam();
+        exams.Add(exam.CreatedExam(exam));
+        foreach (var item in exams)
+        {
+            item.GetExam(exams);
+        }
+    }
+}
---
+++ LabTask28/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace LabTask28;
+
+public class Student
+{
+   public string Name { get; set; }
+    public string SurName { get; set; }
+}
